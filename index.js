874309730ca15a4722c1a771285cf184934b03fc
Mohyminul Islam <mohyminulmunna23@gmail.com>
2026-02-19 12:13:33 +0600

    deploy on vercel

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     const database = client.db("ZapShift");
     const usersCollection = database.collection("users");
     const parcelsCollection = database.collection("parcels");
@@ -369,7 +369,7 @@ async function run() {
       }
     );
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("✅ Successfully connected to MongoDB!");
   } finally {
   }
